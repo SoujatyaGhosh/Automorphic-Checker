589114d7c88c820b758f3289d18211feb116eaaa
Soujatya Ghosh <129378554+SoujatyaGhosh@users.noreply.github.com>
2023-09-13 13:46:30 +0530

    Add files via upload

---
+++ AutomorphicNo.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class AutomorphicNo {
+    public static void main(String[]args) {
+        int num = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");   
+        int n = sc.nextInt();
+        while(n != 0) 
+        {
+            int r = n %10;
+            num++;
+            n=n/10;
+        }
+        int sq = n*n;
+        int m = (int)Math.pow(10, num);
+        if (sq % m == n) 
+        {
+            System.out.println(" The number is automorphic.");
+        }
+        else
+        {
+            System.out.println("The number is not automorphic");
+        }
+    }
+}
